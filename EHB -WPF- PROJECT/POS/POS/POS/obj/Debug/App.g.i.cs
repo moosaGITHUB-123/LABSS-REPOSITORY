d66b91c4ce55e986a32dd7f5a215cf4c79d9ad61
MOOSA GIT ACCOUNT <moosa122333@gmail.com>
2020-12-21 02:02:59 +0500

    [moosa]
    POS LOGIN COMPLETE

--- EHB -WPF- PROJECT/POS/POS/POS/obj/Debug/App.g.i.cs
+++ EHB -WPF- PROJECT/POS/POS/POS/obj/Debug/App.g.i.cs
@@ -9,6 +9,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
+using DevExpress.Xpf.DXBinding;
 using POS;
 using System;
 using System.Diagnostics;
