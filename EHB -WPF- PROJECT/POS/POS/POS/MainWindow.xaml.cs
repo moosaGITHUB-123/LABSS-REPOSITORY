1287fee298bd89c88fe0edf8eb8b5471460e17a0
MOOSA GIT ACCOUNT <moosa122333@gmail.com>
2020-12-27 02:53:07 +0500

    [moosa]
    ui finialzation and app config working on POS

--- EHB -WPF- PROJECT/POS/POS/POS/MainWindow.xaml.cs
+++ EHB -WPF- PROJECT/POS/POS/POS/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+
 
 namespace POS
 {
@@ -20,9 +23,106 @@ namespace POS
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string sqlinst;
+        public string DB;
+        public string DBpass;
+        static string constr = ConfigurationManager.ConnectionStrings["POS.Properties.Settings.Setting"].ConnectionString;
+        SqlConnection con = new SqlConnection(constr);
+
+
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void LOGINButton_Copy1_Click(object sender, RoutedEventArgs e)
+        {
+            //sqlinst = txtSqlInst.Text;
+            //DB = txtDB.Text;
+            //DBpass = txtDBPass.Text;
+           var consucessful = "------- Connectivity Succeeded ✅ --------";
+
+           
+            try
+            {
+                con.Open();
+                MessageBox.Show(consucessful);
+               
+
+            }
+            catch(Exception exceptions) {
+                
+                MessageBox.Show("------ 404 CONNECTION ERROR --------"+exceptions.ToString());
+            }
+            con.Close();
+        }
+
+        private void SimpleButton4_Click(object sender, RoutedEventArgs e)
+        {
+            //  MainWindow = new MainWindow();
+            this.Close();
+        }
+
+        private void SimpleButton2_Click(object sender, RoutedEventArgs e)
+        {
+            //this.WindowState = Properties.Settings.Default.X1State;
+            // this.WindowState{m};
+            // this.WindowState{ Height = 1024; width = 768; };
+            //MainWindow = new MainWindow();
+           // POS.MainWindow.wid
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            //if (TxtLoginID.Text == "PLEASE LOGIN HERE . . ." && txtLoginPassword.Text == "ENTER PASSWORD. . .")
+            //{
+            //    TxtLoginID.Text = "";
+            //    txtLoginPassword.Text = "";
+            //}
+
+           
+            //else
+            //{
+            //    TxtLoginID.Text = "PLEASE LOGIN HERE . . .";
+            //    txtLoginPassword.Text = "ENTER PASSWORD . . .";
+            //}
+        }
+
+        private void TxtLoginID_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (TxtLoginID.Text.Contains("PLEASE LOGIN HERE . . .") && txtLoginPassword.Text.Contains("ENTER PASSWORD. . ."))
+            {
+                TxtLoginID.Text = "";
+                txtLoginPassword.Text = "";
+            }
+            else
+            { };
+
+            //else
+            //{
+            //    TxtLoginID.Text = "PLEASE LOGIN HERE . . .";
+            //    txtLoginPassword.Text = "ENTER PASSWORD . . .";
+            //}
+        }
+
+        private void TxtLoginPassword_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (TxtLoginID.Text.Contains("PLEASE LOGIN HERE . . .") && txtLoginPassword.Text.Contains("ENTER PASSWORD. . ."))
+            {
+                TxtLoginID.Text = "";
+                txtLoginPassword.Text = "";
+            }
+            else { };
+
+
+
+            //else
+            //{
+            //    TxtLoginID.Text = "PLEASE LOGIN HERE . . .";
+            //    txtLoginPassword.Text = "ENTER PASSWORD . . .";
+            //}
         }
     }
 }
