0b24c791c616d74007a1fb38b8af653e2ae6898d
MOOSA GIT ACCOUNT <moosa122333@gmail.com>
2021-01-03 14:42:48 +0500

    moosa
    [--latstt working pushed ]

---
+++ EHB -WPF- PROJECT/ERP/POS/POS/MainWindow.xaml.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace POS
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string sqlinst;
+        public string DB;
+        public string DBpass;
+        static string constr = ConfigurationManager.ConnectionStrings["POS.Properties.Settings.Setting"].ConnectionString;
+        SqlConnection con = new SqlConnection(constr);
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void LOGINButton_Copy1_Click(object sender, RoutedEventArgs e)
+        {
+            //sqlinst = txtSqlInst.Text;
+            //DB = txtDB.Text;
+            //DBpass = txtDBPass.Text;
+           var consucessful = "------- Connectivity Succeeded ✅ --------";
+
+           
+            try
+            {
+                using (SqlConnection con = new SqlConnection(constr))
+                {
+
+              
+                    con.Open();
+                MessageBox.Show(consucessful);
+                con.Close();
+                }
+
+            }
+            catch(Exception exceptions) {
+                
+                MessageBox.Show("------ 404 CONNECTION ERROR --------"+exceptions.ToString());
+            }
+           
+        }
+
+        private void SimpleButton4_Click(object sender, RoutedEventArgs e)
+        {
+            //  MainWindow = new MainWindow();
+            this.Close();
+        }
+
+        private void SimpleButton2_Click(object sender, RoutedEventArgs e)
+        {
+         
+        }
+
+        private void BtnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (TxtLoginID.Text.Trim() != "" && txtLoginPassword.Text.Trim()!="")
+                {
+                    using (con)
+                    {
+                        con.Open();
+                        //DataTable dt = new DataTable();
+                        SqlCommand cmd = new SqlCommand("UserLoginChecking", con);
+                        cmd.CommandType = CommandType.StoredProcedure;
+                    //    cmd.Parameters.Add(new SqlParameter("@UserName", UserName));
+
+                        using (SqlDataReader rdr = cmd.ExecuteReader()) {
+                            while (rdr.Read()) {
+                                MessageBox.Show("Readerssssssssss");
+                            }
+
+                        }
+
+                    }
+                }
+
+                else
+                {
+                    MessageBox.Show("Information: Invalid login Id and Password!!!!!!!");
+                }
+
+                
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Error: "+ex.Message);
+            }
+        }
+
+        private void SimpleButtonMove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SimpleButtonMove_Click_1(object sender, RoutedEventArgs e)
+        {
+            //WindowChangeDatabase wcd1 = new WindowChangeDatabase();
+            //wcd1.ShowDialog();
+        }
+
+        private void SimpleButtonMove_Click_2(object sender, RoutedEventArgs e)
+        {
+            WindowChangeDatabase wcd1 = new WindowChangeDatabase();
+            wcd1.ShowDialog();
+        }
+
+        private void LOGINButton_Copy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+    }
+
---
+++ EHB -WPF- PROJECT/ERP/POS/POS/WindowChangeDatabase.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace POS
+{
+    /// <summary>
+    /// Interaction logic for WindowChangeDatabase.xaml
+    /// </summary>
+    public partial class WindowChangeDatabase : Window
+    {
+        public WindowChangeDatabase()
+        {
+            InitializeComponent();
+
+           
+           
+        }
+
+        private void SimpleButton4_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnLoginSetDB_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = string.Format("DataSource={0};Initial Catalog=(1);User ID=(2);Password=(3);", cboLoginServer.Text, txtLoginDB.Text, txtLoginDBUser.Text, txtLoginDBPass.Text); 
+            try
+            {
+                SqlHelper helper = new SqlHelper(connectionString);
+                if (helper.IsConnection)
+                    MessageBox.Show("Test Connection Succedded . . . . ", "Messege", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Messege", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void BtnLoginSavetDB_Click(object sender, RoutedEventArgs e)
+        {
+            string connectionString = string.Format("DataSource={0};Initial Catalog=(1);User ID=(2); Password=(3);", cboLoginServer.Text, txtLoginDB.Text, txtLoginDBUser.Text, txtLoginDBPass.Text);
+            try
+            {
+                SqlHelper helper = new SqlHelper(connectionString);
+                if (helper.IsConnection)
+                {
+                    AppSetting setting = new AppSetting();
+                    setting.SaveConnectionString("con", connectionString);
+                    MessageBox.Show("CONNECTION SUCCESSFULLY SAVED . . . ", "Messege", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Messege", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+
+        }
+
+        private void Window_Initialized(object sender, EventArgs e)
+        {
+            cboLoginServer.Items.Add(".");
+            cboLoginServer.Items.Add("(local)");
+            cboLoginServer.Items.Add(@".\SQLEXPRESS");
+            cboLoginServer.Items.Add(string.Format(@"{0}\SQLEXPRESS", Environment.MachineName));
+            cboLoginServer.SelectedIndex = 3;
+        }
+    }
+}
