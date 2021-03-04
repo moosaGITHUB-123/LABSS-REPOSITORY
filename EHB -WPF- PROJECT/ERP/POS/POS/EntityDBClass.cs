a9fe8b80e7a308c14b0f7e309938d002229a7a3e
MOOSA GIT ACCOUNT <moosa122333@gmail.com>
2021-03-04 22:57:07 +0500

    [ Moosa ] [ added new clasese for stored prcoedure workings.. ]

---
+++ EHB -WPF- PROJECT/ERP/POS/POS/EntityDBClass.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace POS
+{
+    class EntityDBClass
+    {
+        static string constr = ConfigurationManager.ConnectionStrings["POS.Properties.Settings.Setting"].ConnectionString;
+        public static int Create(string spname, string[] parametername, string[] parametervalue)
+        {
+            SqlConnection connection = new SqlConnection(constr);
+            connection.Open();
+            string query = "EXECUTE " + spname + " ";
+            for(int i =0; i < parametername.Count(); i++)
+            {
+                query = query + parametername[i].ToString() + "=";
+                int n;
+                bool isint = int.TryParse(parametervalue[i], out n);
+                if (isint == true)
+                {
+                    query = query + parametervalue[i] + ", ";
+                }else
+                {
+                    decimal d;
+                    bool isdecimal = decimal.TryParse(parametervalue[i], out d);
+                    if (isdecimal == true)
+                    {
+                        query = query + parametervalue[i] + ", ";
+                    }else
+                    {
+                        bool x;
+                        bool isboolean = bool.TryParse(parametervalue[i], out x);
+                        if (x == true)
+                        {
+                            query = query + parametervalue[i] + ", ";
+                        }
+                        else
+                        {
+                            query = query +"'" +parametervalue[i] + "', ";
+                        }
+
+                    }
+                }
+            }
+            string queryx = query.Remove(query.Length - 2, 1);
+            SqlCommand command = new SqlCommand(queryx,
+              connection);
+
+            command.ExecuteNonQuery();
+            
+            int newid = Convert.ToInt32(command.ExecuteScalar());
+            connection.Close();
+            return newid;
+        }
+
+        public static bool Update(string spname, string[] parametername, string[] parametervalue)
+        {
+            SqlConnection connection = new SqlConnection(constr);
+            connection.Open();
+            string query = "EXECUTE " + spname + " ";
+            for (int i = 0; i < parametername.Count(); i++)
+            {
+                query = query + parametername[i].ToString() + "=";
+                int n;
+                bool isint = int.TryParse(parametervalue[i], out n);
+                if (isint == true)
+                {
+                    query = query + parametervalue[i] + ", ";
+                }
+                else
+                {
+                    decimal d;
+                    bool isdecimal = decimal.TryParse(parametervalue[i], out d);
+                    if (isdecimal == true)
+                    {
+                        query = query + parametervalue[i] + ", ";
+                    }
+                    else
+                    {
+                        bool x;
+                        bool isboolean = bool.TryParse(parametervalue[i], out x);
+                        if (x == true)
+                        {
+                            query = query + parametervalue[i] + ", ";
+                        }
+                        else
+                        {
+                            query = query + "'" + parametervalue[i] + "', ";
+                        }
+
+                    }
+                }
+            }
+            string queryx = query.Remove(query.Length - 2, 1);
+            SqlCommand command = new SqlCommand(queryx,
+              connection);
+
+            int a = command.ExecuteNonQuery();
+            connection.Close();
+            if (a == 0)
+                return false;
+            else
+                return true;
+        }
+
+
+        public static DataTable Select(string spname, string[] parametername, string[] parametervalue)
+        {
+            SqlConnection connection = new SqlConnection(constr);
+            connection.Open();
+            string query = "EXECUTE " + spname + " ";
+            for (int i = 0; i < parametername.Count(); i++)
+            {
+                query = query + parametername[i].ToString() + "=";
+                int n;
+                bool isint = int.TryParse(parametervalue[i], out n);
+                if (isint == true)
+                {
+                    query = query + parametervalue[i] + ", ";
+                }
+                else
+                {
+                    decimal d;
+                    bool isdecimal = decimal.TryParse(parametervalue[i], out d);
+                    if (isdecimal == true)
+                    {
+                        query = query + parametervalue[i] + ", ";
+                    }
+                    else
+                    {
+                        bool x;
+                        bool isboolean = bool.TryParse(parametervalue[i], out x);
+                        if (x == true)
+                        {
+                            query = query + parametervalue[i] + ", ";
+                        }
+                        else
+                        {
+                            query = query + "'" + parametervalue[i] + "', ";
+                        }
+
+                    }
+                }
+            }
+            string queryx = query.Remove(query.Length - 2, 1);
+            SqlCommand command = new SqlCommand(queryx,
+              connection);
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            // this will query your database and return the result to your datatable
+            da.Fill(dt);
+            connection.Close();
+            da.Dispose();
+            return dt;
+           
+        }
+
+      
+
+    }
+}
--- EHB -WPF- PROJECT/ERP/POS/POS/WindowProductCategory.xaml.cs
+++ EHB -WPF- PROJECT/ERP/POS/POS/WindowProductCategory.xaml.cs
@@ -1,25 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using System.Configuration;
 using System.Data.SqlClient;
-using System.Data;
-using System.Collections.ObjectModel;
-using POS.DBClasses;
-using DevExpress.Xpf.Charts;
-using DevExpress.Xpf;
-using DevExpress.Xpf.Docking;
-using DevExpress.Xpf.Core;
 
 
 namespace POS
@@ -39,82 +20,74 @@ namespace POS
 
         private void BtnSave_Click(object sender, RoutedEventArgs e)
         {
-            
-                try
-                {
-                    ///////////  Login  Sp Calling Below   ////////////
 
-                    con.Open();
-
-                string querymaster = "Exec spProductCategory @Type=1, @SelectedCategoryID=0,@CategoryFullName='"+ CategoryFullName.Text + "',@CategoryShortName='"+ CategoryShortName.Text + "' ";
-                SqlCommand cmd = new SqlCommand(querymaster, con);
-//                    cmd.CommandType = CommandType.StoredProcedure;
-//                    cmd.Parameters.AddWithValue("@Type", SqlDbType.Int).Value = 1;
-//                    cmd.Parameters.AddWithValue("@SelectedCategoryID", SqlDbType.VarChar).Value = 0;
-//                    cmd.Parameters.AddWithValue("@CategoryFullName", SqlDbType.VarChar).Value = CategoryFullName.Text;
-//                    cmd.Parameters.AddWithValue("@CategoryShortName", SqlDbType.VarChar).Value = CategoryShortName.Text;
-//                    cmd.Parameters.AddWithValue("@CategoryDescription", SqlDbType.VarChar).Value = CategoryDescription.Text.Trim();
-//                    cmd.Parameters.AddWithValue("@CategoryParentID", SqlDbType.VarChar).Value = int.Parse(CategoryParent.Text.Trim());
-////------------------------------------------------------
-//                    cmd.Parameters.AddWithValue("@recordStatusID", SqlDbType.VarChar).Value = 1;
-//                    cmd.Parameters.AddWithValue("@recordStatusCause", SqlDbType.VarChar).Value = "NO CAUSE";
-//                    cmd.Parameters.AddWithValue("@recordLastModifiedBy", SqlDbType.VarChar).Value = 0;
-//                    cmd.Parameters.AddWithValue("@recordLastModifiedDate", SqlDbType.VarChar).Value = "2021-01-01 00:00:00.000";
-//                    cmd.Parameters.AddWithValue("@recordEntryBy", SqlDbType.VarChar).Value = 0;
-//                    cmd.Parameters.AddWithValue("@recordEntryDate", SqlDbType.DateTime).Value = "2021-01-01 00:00:00.000";
-                DataTable dt = new DataTable();
-                SqlDataAdapter da = new SqlDataAdapter(cmd);
-                // this will query your database and return the result to your datatable
-                da.Fill(dt);
-                con.Close();
-                da.Dispose();
-                if (dt != null && dt.Rows.Count > 0)
-                {
-                    if (dt.Rows[0][0] != null)
-                    {
-                        using (SqlConnection conn = new SqlConnection(constr))
-                        {
+            int id = EntityDBClass.Create("spProductCategory", new string[] {"@Type","@CategoryFullName","@CategoryParentID","@CategoryShortName","@CategoryDescription"
+            }, new string[] { "1", TxtCategoryFullName.Text.ToString(), CmbCategoryParent.SelectedValue.ToString(), TxtCategoryShortName.Text.ToString(), TxtCategoryDescription.Text.ToString() });
 
 
-                            conn.Open();
-                            string query = "Exec spProductCategory @Type=5, @SelectedCategoryID=" + Convert.ToInt32(dt.Rows[0][0]);
-                            SqlCommand cmd2 = new SqlCommand(query,conn);
-                          
-                            DataTable dt1 = new DataTable();
-                            SqlDataAdapter da1 = new SqlDataAdapter(cmd2);
-                            // this will query your database and return the result to your datatable
-                            da1.Fill(dt1);
 
-                            da1.Dispose();
-                            conn.Close();
-                        }
-                    }
-                }
-                }
-
-
-                catch (Exception ex) { MessageBox.Show(ex.ToString());
-                con.Close();
-            }
-
-            
         }
 
         private void BtnNew_Click(object sender, RoutedEventArgs e)
         {
             //----------
-            CategoryID.Content = "";
-            CategoryFullName.Text = "";
-            CategoryShortName.Text = "";
-            CategoryDescription.Text = "";
-            Status.Text = "";
-            StatusReason.Text = "";
+            lblCategoryID.Content = "";
+            TxtCategoryFullName.Text = "";
+            TxtCategoryShortName.Text = "";
+            TxtCategoryDescription.Text = "";
+            CmbStatus.Text = "";
+            TxtRecordStatusCause.Text = "";
 
-            CategoryParent.Items.Add("1");
-            CategoryParent.SelectedIndex = 1;
-            Status.Items.Add("1");
-            Status.SelectedIndex = 1;
+            CmbCategoryParent.Items.Add("1");
+            CmbCategoryParent.SelectedIndex = 1;
+            CmbStatus.Items.Add("1");
+            CmbStatus.SelectedIndex = 1;
             //-------------
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            using (SqlConnection con3 = new SqlConnection(constr))
+            {
+
+
+                //  con3.Open();
+
+                ////  SqlCommand cmd3 = new SqlCommand("spProductCategory", con3);
+                ////  cmd3.CommandType = CommandType.StoredProcedure;
+                ////  cmd3.Parameters.AddWithValue("@Type", SqlDbType.Int).Value = 2;
+                //    string query = "Exec spProductCategory @Type=2" ;
+                //   SqlCommand cmd3 = new SqlCommand(query, con3);
+
+                //  DataTable dt3 = new DataTable();
+
+                //  SqlDataReader rd = cmd3.ExecuteReader();
+                //  if (rd.Read())
+                //  {
+
+                //      // this will query your database and return the result to your datatable
+                //      //rd.Fill(dt3);
+
+                //      //da3.Dispose();
+                //      //con3.Close();
+
+                //con3.Open();
+
+                //SqlCommand cmd = new SqlCommand("ProductCategory", con3);
+                //cmd.CommandType = CommandType.StoredProcedure;
+                //cmd.Parameters.AddWithValue("@CategoryID", SqlDbType.VarChar).Value = null;
+                //cmd.Parameters.AddWithValue("@type", SqlDbType.VarChar).Value = 0;
+                //SqlDataReader rd3 = cmd.ExecuteReader();
+                //DataTable dt3 = new DataTable();
+                //if (rd3.Read())
+                //{
+
+                    
+                //}
+                //con3.Close();
+
+                
+            }
         }
     }
 }
