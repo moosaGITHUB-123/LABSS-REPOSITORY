09c55894e64cc2320cd7107ed53589b0b6e1609a
MOOSA GIT ACCOUNT <moosa122333@gmail.com>
2021-03-13 23:19:26 +0500

    added category class in last calss

---
+++ EHB -WPF- PROJECT/ERP/POS/POS/DBClasses/ProductCategory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.DBClasses
+{
+    public class ProductCategory
+    {
+        public int CategoryID { get; set; }
+
+        public string CategoryParentID { get; set; }
+
+        public string CategoryFullName { get; set; }
+
+        public string CategoryShortName { get; set; }
+
+        public string CategoryDescription { get; set; }
+
+        public int? recordStatusID { get; set; }
+
+        public string recordStatusCause { get; set; }
+
+        public int? recordLastModifiedBy { get; set; }
+
+        public DateTime? recordLastModifiedDate { get; set; }
+
+        public int? recordEntryBy { get; set; }
+
+        public DateTime? recordEntryDate { get; set; }
+
+        public string recordStatusName { get; set; }
+
+    }
+
+}
