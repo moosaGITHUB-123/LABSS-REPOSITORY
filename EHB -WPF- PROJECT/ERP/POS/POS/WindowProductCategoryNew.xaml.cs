6501d03963f8b3200fe9c4508139918c08304da9
MOOSA GIT ACCOUNT <moosa122333@gmail.com>
2021-03-13 21:45:30 +0500

    [last working in class]

--- EHB -WPF- PROJECT/ERP/POS/POS/WindowProductCategoryNew.xaml.cs
+++ EHB -WPF- PROJECT/ERP/POS/POS/WindowProductCategoryNew.xaml.cs
@@ -11,6 +11,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using POS.DBClasses;
 
 namespace POS
 {
@@ -24,6 +27,32 @@ namespace POS
             InitializeComponent();
         }
 
-       
+        private void BtnNew_Click(object sender, RoutedEventArgs e)
+        {
+
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            DataTable dt = EntityDBClass.Select("spProductCategory" , new string[] { "@Type" }, new string[] { "9" });
+            IList<ProductCategory> items = dt.AsEnumerable().Select(row =>
+    new ProductCategory
+    {
+        CategoryID = row.Field<int>("CategoryID"),
+        CategoryParentID = row.Field<string>("CategoryParentID")==DBNull.Value?0 : row.Field<string>("CategoryParentID"),
+        CategoryFullName = row.Field<string>("CategoryFullName"),
+        CategoryShortName = row.Field<string>("CategoryShortName"),
+        CategoryDescription = row.Field<string>("CategoryDescription"),
+        recordStatusID = row.Field<int>("recordStatusID"),
+        recordStatusCause = row.Field<string>("recordStatusCause"),
+        recordLastModifiedBy = row.Field<int>("recordLastModifiedBy"),
+        recordLastModifiedDate = row.Field<DateTime>("recordLastModifiedDate"),
+        recordEntryBy = row.Field<int>("recordEntryBy"),
+        recordEntryDate = row.Field<DateTime>("recordEntryDate"),
+        recordStatusName = row.Field<string>("recordStatusName"),
+
+    }).ToList();
+        }
     }
 }
