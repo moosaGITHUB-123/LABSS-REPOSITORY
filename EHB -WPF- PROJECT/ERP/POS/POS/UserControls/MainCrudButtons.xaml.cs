4ae10d822f2e8c597bc47bfdad09cd4bf251aabb
MOOSA GIT ACCOUNT <moosa122333@gmail.com>
2021-03-15 22:11:09 +0500

    [  last working in class related to control and other working ...]

---
+++ EHB -WPF- PROJECT/ERP/POS/POS/UserControls/MainCrudButtons.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace POS.UserControls
+{
+    /// <summary>
+    /// Interaction logic for MainCrudButtons.xaml
+    /// </summary>
+    public partial class MainCrudButtons : UserControl
+    {
+        public MainCrudButtons()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
--- EHB -WPF- PROJECT/ERP/POS/POS/WindowProductCategoryNew.xaml.cs
+++ EHB -WPF- PROJECT/ERP/POS/POS/WindowProductCategoryNew.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Shapes;
 using System.Data;
 using System.Data.SqlClient;
 using POS.DBClasses;
+using System.Collections.ObjectModel;
+
 
 namespace POS
 {
@@ -27,32 +29,59 @@ namespace POS
             InitializeComponent();
         }
 
-        private void BtnNew_Click(object sender, RoutedEventArgs e)
-        {
 
-
-        }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            DataTable dt = EntityDBClass.Select("spProductCategory" , new string[] { "@Type" }, new string[] { "9" });
-            IList<ProductCategory> items = dt.AsEnumerable().Select(row =>
-    new ProductCategory
-    {
-        CategoryID = row.Field<int>("CategoryID"),
-        CategoryParentID = row.Field<string>("CategoryParentID")==DBNull.Value?0 : row.Field<string>("CategoryParentID"),
-        CategoryFullName = row.Field<string>("CategoryFullName"),
-        CategoryShortName = row.Field<string>("CategoryShortName"),
-        CategoryDescription = row.Field<string>("CategoryDescription"),
-        recordStatusID = row.Field<int>("recordStatusID"),
-        recordStatusCause = row.Field<string>("recordStatusCause"),
-        recordLastModifiedBy = row.Field<int>("recordLastModifiedBy"),
-        recordLastModifiedDate = row.Field<DateTime>("recordLastModifiedDate"),
-        recordEntryBy = row.Field<int>("recordEntryBy"),
-        recordEntryDate = row.Field<DateTime>("recordEntryDate"),
-        recordStatusName = row.Field<string>("recordStatusName"),
+            DataTable dt = EntityDBClass.Select("spProductCategory", new string[] { "@Type" }, new string[] { "9" });
 
-    }).ToList();
+            ObservableCollection<ProductCategory> test = new ObservableCollection<ProductCategory>();
+            foreach (DataRow row in dt.Rows)
+            {
+                var obj = new ProductCategory()
+                {
+                    CategoryID = (int)row["CategoryID"],
+                    CategoryParentID = row["CategoryParentID"] ==DBNull.Value? "": (string)row["CategoryParentID"],
+                    CategoryFullName = row["CategoryFullName"] == DBNull.Value ? "" : (string)row["CategoryFullName"],
+                    CategoryShortName = row["CategoryShortName"] == DBNull.Value ? "" : (string)row["CategoryShortName"],
+                    CategoryDescription = row["CategoryDescription"] == DBNull.Value ? "" : (string)row["CategoryDescription"],
+                    recordStatusID = row["recordStatusID"] == DBNull.Value ? 0 : (int)row["recordStatusID"],
+                    recordStatusCause = row["recordStatusCause"] == DBNull.Value ? "" : (string)row["recordStatusCause"],
+                    recordLastModifiedBy = row["recordLastModifiedBy"] == DBNull.Value ? 0 : (int)row["recordLastModifiedBy"],
+                    recordLastModifiedDate = row["recordLastModifiedDate"] == DBNull.Value ? DateTime.MinValue : (DateTime)row["recordLastModifiedDate"],
+                    recordEntryBy = row["recordEntryBy"] == DBNull.Value ? 0 : (int)row["recordEntryBy"],
+                    recordEntryDate = row["recordEntryDate"] == DBNull.Value ? DateTime.MinValue : (DateTime)row["recordEntryDate"],
+                    recordStatusName = row["recordStatusName"] == DBNull.Value ? "" : (string)row["recordStatusName"],
+
+                };
+                test.Add(obj);
+
+            }
+            
+           var ParentCategory = test.Where(x => x.CategoryParentID == "").ToList<ProductCategory>();
+
+            foreach (var pcat in ParentCategory)
+            {
+
+                //if (!Page.IsPostBack)
+                //{
+                //    AccordionPane ap1 = new AccordionPane();
+                //    ap1.HeaderContainer.Controls.Add(new LiteralControl("Using Markup"));
+                //    ap1.ContentContainer.Controls.Add(new
+                //    LiteralControl("Adding panes using markup is really simple."));
+                //    AccordionPane ap2 = new AccordionPane();
+                //    ap2.HeaderContainer.Controls.Add(new LiteralControl("Using Code"));
+                //    ap2.ContentContainer.Controls.Add(new
+                //    LiteralControl("Adding panes using code is really flexible."));
+                //    acc1.Panes.Add(ap1);
+                //    acc1.Panes.Add(ap2);
+                //}
+
+             //
+                CategoryTree.Items.Add(pcat.CategoryFullName);
+            }
+
+
         }
     }
 }
