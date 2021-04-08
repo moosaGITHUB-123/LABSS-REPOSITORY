2560d8c2b4d07f7df58632915f4c3a1cb2c2a6fd
MOOSA GIT ACCOUNT <moosa122333@gmail.com>
2021-04-09 00:01:09 +0500

    ehtsham

---
+++ EHB -WPF- PROJECT/ERP/POS/POS/DBClasses/spProductComboBoxLoad.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.DBClasses
+{
+    public class spProductComboBoxLoad
+    {
+        public int ID { get; set; }
+
+        public string ComboValue { get; set; }
+
+        // public string ComboValue2 { get; set; }
+
+    }
+
+}
